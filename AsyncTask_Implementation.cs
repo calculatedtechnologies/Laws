f62048bb9fff9ed69be51fa413928aa90d920089
Calculated Technologies <51523286+calculatedtechnologies@users.noreply.github.com>
2019-12-16 16:48:24 -0500

    Implementation added

--- AsyncTask_Implementation.cs
+++ AsyncTask_Implementation.cs
@@ -1,15 +1,27 @@
-//Create Asyncronous Task efficiently.
+//Create Asyncronous Tasks efficiently.
 	
-//Function
 //Identify a function that returns a value
+public double ExampleFunc(double a, double b)
+{
+	return a + b;
+}
 
-//Task
 //Create a async function that returns a Task
+private Task<double> asyncExampleFunc(double a, double b)
+{
+	return Task<double>.Run(() =>
+	{
+		return ExampleFunc(a, b);
+	});
+}
 
-//Await the identified function inside of the async function
 
-//Call
-//Call the async function to start the asyncronous task	
+//Call the async function to await the asyncronous task	
+public async void CallerFunc(double a, double b)
+{
+	//Await the identified function inside of the async function
+	await (asyncExampleFunc(a,b));
+}
 
 //Test
 //Write Unit Test that asserts that the async function returns a Task
