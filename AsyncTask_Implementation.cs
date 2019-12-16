b20a7183d1ab982b838a9fa638276a371cba74ce
Calculated Technologies <51523286+calculatedtechnologies@users.noreply.github.com>
2019-12-16 16:37:11 -0500

    Create

---
+++ AsyncTask_Implementation.cs
@@ -0,0 +1,17 @@
+//Create Asyncronous Task efficiently.
+	
+//Function
+//Identify a function that returns a value
+
+//Task
+//Create a async function that returns a Task
+
+//Await the identified function inside of the async function
+
+//Call
+//Call the async function to start the asyncronous task	
+
+//Test
+//Write Unit Test that asserts that the async function returns a Task
+		
+//Snippet
